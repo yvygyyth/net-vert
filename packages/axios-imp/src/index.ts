491117bc6ff10ffe53050dae97399fe0a0a7e918
虞子楠 <12227713+yu-zinan@user.noreply.gitee.com>
2025-03-25 15:47:47 +0800

    feat: axios-imp

--- packages/axios-imp/src/index.ts
+++ packages/axios-imp/src/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import type { AxiosRequestConfig } from 'axios'
-import type { UnifiedRequestor } from '@net-vert/core'
+import type { UnifiedRequestor, UnifiedConfig } from '@net-vert/core'
 
 const instance = axios.create({
   baseURL: '/api',
   timeout: 60000
 })
 
-export const requestor: UnifiedRequestor = (config) => {
+export const requestor: UnifiedRequestor = (config: UnifiedConfig) => {
   return instance.request(config as AxiosRequestConfig)
 }
--- packages/axios-imp/vite.config.ts
+++ packages/axios-imp/vite.config.ts
@@ -16,7 +16,17 @@ export default defineConfig({
       // the proper extensions will be added
       fileName: 'index',
       formats: ['es', 'umd']
-    }
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['axios'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'axios'
+        },
+      },
+    },
   },
   plugins: [
     dts({
