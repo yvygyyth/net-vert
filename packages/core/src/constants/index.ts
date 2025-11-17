124175ee34eafaa9338607d945e0184f0da8c346
yvygyyth <1589937631@qq.com>
2025-11-17 16:54:56 +0800

    chore: clean up TypeScript configuration and constants formatting

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -2,19 +2,19 @@ export enum REQUEST_METHOD {
     GET = 'get',
     POST = 'post',
     PUT = 'put',
-    DELETE = 'delete',
+    DELETE = 'delete'
 }
 
-export type RequestMethod = REQUEST_METHOD;
+export type RequestMethod = REQUEST_METHOD
 
 export enum MIDDLEWARE_TYPE {
     CACHE = 'cache',
     RETRY = 'retry',
     IDEMPOTENT = 'idempotent',
     CONCURRENT = 'concurrent',
-    SYNC = 'sync',
+    SYNC = 'sync'
 }
 
-export type MiddlewareType = MIDDLEWARE_TYPE;
+export type MiddlewareType = MIDDLEWARE_TYPE
 
 export const DEFAULT_KEY = 'default'
