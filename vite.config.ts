4b0f3466762a84d00c244aa2ef937160b7f942a8
yuzinan <1589937631@qq.com>
2026-04-03 15:53:40 +0800

    chore: update dts plugin configuration to include src/index.ts and exclude test files

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,40 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: 'src/index',
-      name: 'netVertCore',
-      // the proper extensions will be added
-      fileName: 'index',
-      formats: ['es', 'umd']
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-    rollupOptions: {
-      external: ['id-queue', 'localforage'], // 关键配置
-      output: {
-        globals: {
-          'id-queue': 'idQueue',
-          'store-vert': 'store-vert'
-        }
-      }
-    }
-  },
-  plugins: [
-    dts({
-      tsconfigPath: 'tsconfig.app.json',
-      // include: 'src/index',
-      exclude: ['**/node_modules'],
-      copyDtsFiles: true,
-      rollupTypes: true
-    })
-  ],
-  base: '/'
-})
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: 'src/index',
+            name: 'netVertCore',
+            // the proper extensions will be added
+            fileName: 'index',
+            formats: ['es', 'umd'],
+        },
+        rollupOptions: {
+            external: ['id-queue', 'localforage'], // 关键配置
+            output: {
+                globals: {
+                    'id-queue': 'idQueue',
+                    'store-vert': 'store-vert',
+                },
+            },
+        },
+    },
+    plugins: [
+        dts({
+            tsconfigPath: 'tsconfig.app.json',
+            include: ['src/index.ts'],
+            exclude: ['**/node_modules', 'src/__test__/**'],
+            copyDtsFiles: true,
+            rollupTypes: false,
+        }),
+    ],
+    base: '/',
+});
